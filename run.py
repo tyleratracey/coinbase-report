60d80e190c95247a6f4249ef499143875bfde107
Tyler Tracey <tylertracey@Tylers-Mac-mini.local>
2021-08-26 13:45:05 -0400

    var names

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def getCurrentCoinPrice(asset):
+def getCurrentUSDCoinPrice(asset):
     response = requests.get('https://min-api.cryptocompare.com/data/price?fsym='+asset+'&tsyms=USD')
     return response.json()['USD']
 
@@ -29,13 +29,12 @@ def getDollarCostAverageWithoutFees(asset, tax, headers, data):
 
     for row in data:
         if row[assetIndex] == asset and row[transactionTypeIndex] == 'Buy':
-            #print(row)
             total += float(row[usdTotalIndex])
             numShares += float(row[quantityTransactedIndex])
 
     if numShares > 0 and total > 0:
-        dca = (total / numShares)
-        currentAssetPrice = getCurrentCoinPrice(asset)
+        dollarCostAverage = (total / numShares)
+        currentAssetPrice = getCurrentUSDCoinPrice(asset)
 
         currentUSDValue = (currentAssetPrice * numShares)
         usdGainLoss = (currentUSDValue - total)
@@ -44,9 +43,9 @@ def getDollarCostAverageWithoutFees(asset, tax, headers, data):
         takeHome = (usdGainLoss - taxesOwed)
 
         print(f'{bcolors.HEADER}{bcolors.BOLD}{bcolors.UNDERLINE}### {asset} REPORT ###{bcolors.ENDC}\n')
-        print(f'You purchased {bcolors.WARNING}{numShares}{bcolors.ENDC} ({bcolors.WARNING}${total:.2f} USD{bcolors.ENDC}) units of {asset} for an average of {bcolors.WARNING}${dca:.2f} USD{bcolors.ENDC} per share.\n')
+        print(f'You purchased {bcolors.WARNING}{numShares}{bcolors.ENDC} ({bcolors.WARNING}${total:.2f} USD{bcolors.ENDC}) units of {asset} for an average of {bcolors.WARNING}${dollarCostAverage:.2f} USD{bcolors.ENDC} per share.\n')
         print(f'{bcolors.OKCYAN}Amount Bought:\t\t{bcolors.ENDC} {numShares}')
-        print(f'{bcolors.OKCYAN}Average Price Paid:\t{bcolors.ENDC} {dca:.2f}')
+        print(f'{bcolors.OKCYAN}Average Price Paid:\t{bcolors.ENDC} {dollarCostAverage:.2f}')
         print(f'{bcolors.OKCYAN}Current Price:\t\t{bcolors.ENDC} {currentAssetPrice:.2f}')
         print(f'{bcolors.OKCYAN}Total USD Paid:\t\t{bcolors.ENDC} {total}')
         print(f'{bcolors.OKCYAN}Current USD Value:\t{bcolors.ENDC} {(currentUSDValue):.2f}')
@@ -58,6 +57,7 @@ def getDollarCostAverageWithoutFees(asset, tax, headers, data):
         print(f'{bcolors.HEADER}{bcolors.BOLD}{bcolors.UNDERLINE}### {asset} REPORT ###{bcolors.ENDC}\n')
         print(f'{bcolors.FAIL}Sorry, we didnt find any purchase history for {asset}{bcolors.FAIL}\n\n')
 
+## Main function
 def main():
 
     transactionHistory = open('coinbase-history.csv', 'r')
@@ -69,7 +69,7 @@ def main():
     taxPercentage = float(config['tax_percentage'])
 
     if taxPercentage > 1:
-        print(f'{bcolors.FAIL}Your tax_percentage must be in decimal form example: 0.22')
+        print(f'{bcolors.FAIL}Your tax_percentage must be in decimal form lower than 1. Example: 0.22')
     
     else:
         
@@ -77,8 +77,6 @@ def main():
             getDollarCostAverageWithoutFees(item, taxPercentage, headers, reader)
             transactionHistory.seek(0)
 
-    
-
-
+## Main
 if __name__ == "__main__":
     main()
